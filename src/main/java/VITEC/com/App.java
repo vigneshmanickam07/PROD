0eb4aeacf604024d83aec1e0bdecbd1ab8765806
root <root@ip-172-31-21-236.us-east-2.compute.internal>
2019-11-09 11:25:14 +0000

    Webhook

--- src/main/java/VITEC/com/App.java
+++ src/main/java/VITEC/com/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello maven webhook jenkins World!" );
+	System.out.println( "GitHub Push" );
     }
 }
