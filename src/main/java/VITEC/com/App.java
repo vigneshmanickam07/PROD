a626d69d5275f6002a76c92ec562d1a33bbb051b
vigneshmanickam07 <57437415+vigneshmanickam07@users.noreply.github.com>
2019-11-09 03:08:03 -0800

    Update App.java
    webhook

--- src/main/java/VITEC/com/App.java
+++ src/main/java/VITEC/com/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello maven World!" );
+        System.out.println( "Hello maven webhook World!" );
     }
 }
